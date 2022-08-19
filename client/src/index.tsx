e729f1b31c0b2d8ed6cd0cc886e814032d259b17
Agostina Gavilan <agosgavilan3@gmail.com>
2022-08-19 17:43:03 -0300

    fix

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.REACT_APP_APPI || "http://localhost:3001";
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
