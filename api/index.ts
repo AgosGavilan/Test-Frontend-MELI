1bd815ce52895aa58d1a7e792df1f849390439a7
Agostina Gavilan <agosgavilan3@gmail.com>
2022-08-19 16:12:13 -0300

    fix

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 import app from './src/app';
 const { PORT }  = process.env;
 
-app.listen(PORT, function () {
-    console.log(`App is listening on port ${PORT}!`)
+app.listen(PORT || 3000, function () {
+    console.log(`App is listening on port 3000!`)
 })
